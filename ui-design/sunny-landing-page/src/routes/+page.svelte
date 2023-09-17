05e5c445814afd550fe141d2897601c31b1dae76
tonyx92 <tognolimartino@gmail.com>
2023-09-17 19:34:44 +0200

    esercizio ui sitop web con flex box nav e css grid per gli altri elementi

---
+++ ui-design/sunny-landing-page/src/routes/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+
+</script>
+
+<div class="top-content">
+    <div class="flex-nav">
+        <p>sunnyside</p>
+        <div class="navlinks">
+            <ul>
+                <li>About</li>
+                <li>Services</li>
+                <li>Projects</li>
+                <button>CONTACT</button>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<style>
+
+
+*{
+    box-sizing: border-box;
+    margin:0;
+    padding: 0;
+}
+
+:root{
+    /* colors */
+
+    --soft-red: #fe7867;
+    --yellow: #fad400;
+    --dark-desaturated-cyan: #25564b;
+    --dark-blue: #19536b;
+    --dark-moderate-cyan: #458c7e;
+    --header-bg: #3ebfff;
+    --footer-bg: #8ed0c4;
+    --container-bg: #fffbf8;
+    --very-dark-desaturated-blue: #23303e;
+    --very-drak-grayish-blue: #5a636c;
+    --dark-grayish-blue: #818498;
+    --grayish-blue: #a7abae;
+    --white: #ffffff;
+
+
+    /* fonts */
+    --barlow-font: "Barlow", sans-serif;
+    --frauces-font: "Frauces", serif;
+}
+
+
+:global(body){
+    
+}
+
+.top-content{
+    background-image: url("../assets/images/desktop/image-header.jpg");
+    background-size: cover;
+    background-position: center;
+    height: 44.5rem;
+    
+}
+
+.flex-nav{
+    display: flex;
+    justify-content: space-between;
+    padding: 2rem 2.2rem;
+    max-width: 100%;
+}
+
+.div-navlinks{
+    display: flex;
+   
+    
+}
+
+li{
+display: inline;
+padding: 2rem;
+}
+
+</style>
